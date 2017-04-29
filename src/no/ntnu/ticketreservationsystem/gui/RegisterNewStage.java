c01aa1157b3993fdf59b3bf7b75c04a88eaee741
Unknown <olav.telseth94@hotmail.com>
2017-04-29 14:51:05 +0200

    Made the connection between core and registernewwindow
    hallveis, men må bare få de over til den stasjonære dataen.

--- src/no/ntnu/ticketreservationsystem/gui/RegisterNewStage.java
+++ src/no/ntnu/ticketreservationsystem/gui/RegisterNewStage.java
@@ -5,8 +5,6 @@
  */
 package no.ntnu.ticketreservationsystem.gui;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -26,8 +24,10 @@ public class RegisterNewStage {
 
     Stage stage;
     Scene startScene, passengerScene, pilotScene, crewScene, flightScene, planeScene;
+    GUI gui;
 
     public RegisterNewStage(GUI gui) {
+        this.gui = gui;
         stage = new Stage();
         createStartScene();
         stage.setScene(startScene);
@@ -54,22 +54,22 @@ public class RegisterNewStage {
         Button bt2 = new Button("Register New Crew");
         bt2.setPrefSize(400, 150);
         bt2.setOnAction((ActionEvent) -> {
-        createRegisterCrewScene();
-        stage.setScene(crewScene);
+            createRegisterCrewScene();
+            stage.setScene(crewScene);
         });
 
         Button bt3 = new Button("Register New Pilot");
         bt3.setPrefSize(400, 150);
         bt3.setOnAction((ActionEvent) -> {
-        createRegisterPilotScene();
-        stage.setScene(pilotScene);
+            createRegisterPilotScene();
+            stage.setScene(pilotScene);
         });
 
         Button bt4 = new Button("Register New Flight");
         bt4.setPrefSize(400, 150);
         bt4.setOnAction((ActionEvent) -> {
-        createRegisterFlightScene();
-        stage.setScene(flightScene);
+            createRegisterFlightScene();
+            stage.setScene(flightScene);
         });
 
         Button bt5 = new Button("Register New Plane");
@@ -88,16 +88,16 @@ public class RegisterNewStage {
         grid.setVgap(10);
         grid.setHgap(10);
         grid.setStyle("-fx-font-size: 30;");
-        
+
         TextField name = new TextField();
         name.setPromptText("Enter your first name.");
         name.setPrefColumnCount(10);
         GridPane.setConstraints(name, 0, 0);
-        
+
         TextField lastName = new TextField();
         lastName.setPromptText("Enter your last name.");
         GridPane.setConstraints(lastName, 0, 1);
-        
+
         TextField email = new TextField();
         email.setPrefColumnCount(15);
         email.setPromptText("Enter your email address here.");
@@ -105,20 +105,20 @@ public class RegisterNewStage {
 
         Button submit = new Button("Submit");
         GridPane.setConstraints(submit, 1, 0);
-        
+
         Button clear = new Button("Clear");
         GridPane.setConstraints(clear, 1, 1);
-        
+
         Label label = new Label();
         GridPane.setConstraints(label, 0, 3);
         GridPane.setColumnSpan(label, 2);
 
         //Setting an action for the Submit button
-
         submit.setOnAction((ActionEvent) -> {
             if ((email.getText() != null && !email.getText().isEmpty())) {
                 label.setText(name.getText() + " " + lastName.getText() + ", "
                         + "thank you for registering, you have been added to the system!");
+                gui.getCore().doRegisterPassenger(name.getText(), lastName.getText(), email.getText());
 
             } else {
                 label.setText("You have not registered yet!.");
@@ -126,7 +126,6 @@ public class RegisterNewStage {
         });
 
         //Setting an action for the Clear button
-        
         clear.setOnAction((ActionEvent) -> {
             name.clear();
             lastName.clear();
@@ -136,7 +135,8 @@ public class RegisterNewStage {
         grid.getChildren().addAll(name, lastName, email, submit, clear, label);
         passengerScene = new Scene(grid, 850, 500);
     }
-      private void createRegisterCrewScene() {
+
+    private void createRegisterCrewScene() {
 
         GridPane grid = new GridPane();
         grid.setPadding(new Insets(20, 20, 20, 20));
@@ -144,21 +144,21 @@ public class RegisterNewStage {
         grid.setVgap(10);
         grid.setHgap(10);
         grid.setStyle("-fx-font-size: 30;");
-        
+
         TextField name = new TextField();
         name.setPromptText("Enter your first name.");
         name.setPrefColumnCount(10);
         GridPane.setConstraints(name, 0, 0);
-        
+
         TextField lastName = new TextField();
         lastName.setPromptText("Enter your last name.");
         GridPane.setConstraints(lastName, 0, 1);
-        
+
         TextField email = new TextField();
         email.setPrefColumnCount(15);
         email.setPromptText("Enter your email address here.");
         GridPane.setConstraints(email, 0, 2);
-        
+
         TextField employeeNumber = new TextField();
         employeeNumber.setPrefColumnCount(20);
         employeeNumber.setPromptText("Enter employee ID here.");
@@ -166,20 +166,21 @@ public class RegisterNewStage {
 
         Button submit = new Button("Submit");
         GridPane.setConstraints(submit, 1, 0);
-        
+
         Button clear = new Button("Clear");
         GridPane.setConstraints(clear, 1, 1);
-        
+
         Label label = new Label();
         GridPane.setConstraints(label, 0, 6);
         GridPane.setColumnSpan(label, 2);
 
         //Setting an action for the Submit button
-
         submit.setOnAction((ActionEvent) -> {
             if ((email.getText() != null && !email.getText().isEmpty())) {
                 label.setText(name.getText() + " " + lastName.getText() + ", "
                         + "Welcome to the team, you have been added to the system as a pilot!");
+                gui.getCore().doRegisterCrew(name.getText(), lastName.getText(),
+                        email.getText(), employeeNumber.getText());
 
             } else {
                 label.setText("You have not registered yet!.");
@@ -187,7 +188,6 @@ public class RegisterNewStage {
         });
 
         //Setting an action for the Clear button
-        
         clear.setOnAction((ActionEvent) -> {
             name.clear();
             lastName.clear();
@@ -195,10 +195,11 @@ public class RegisterNewStage {
             employeeNumber.clear();
             label.setText(null);
         });
-        grid.getChildren().addAll(name, lastName, email, employeeNumber,  submit, clear, label);
+        grid.getChildren().addAll(name, lastName, email, employeeNumber, submit, clear, label);
         crewScene = new Scene(grid, 850, 500);
     }
-      private void createRegisterPilotScene() {
+
+    private void createRegisterPilotScene() {
 
         GridPane grid = new GridPane();
         grid.setPadding(new Insets(20, 20, 20, 20));
@@ -206,26 +207,26 @@ public class RegisterNewStage {
         grid.setVgap(10);
         grid.setHgap(10);
         grid.setStyle("-fx-font-size: 30;");
-        
+
         TextField name = new TextField();
         name.setPromptText("Enter your first name.");
         name.setPrefColumnCount(10);
         GridPane.setConstraints(name, 0, 0);
-        
+
         TextField lastName = new TextField();
         lastName.setPromptText("Enter your last name.");
         GridPane.setConstraints(lastName, 0, 1);
-        
+
         TextField email = new TextField();
         email.setPrefColumnCount(15);
         email.setPromptText("Enter your email address here.");
         GridPane.setConstraints(email, 0, 2);
-        
+
         TextField employeeNumber = new TextField();
         employeeNumber.setPrefColumnCount(15);
         employeeNumber.setPromptText("Enter employee ID here.");
         GridPane.setConstraints(employeeNumber, 0, 3);
-        
+
         TextField sertificate = new TextField();
         sertificate.setPrefColumnCount(15);
         sertificate.setPromptText("Enter certificate ID here.");
@@ -233,20 +234,22 @@ public class RegisterNewStage {
 
         Button submit = new Button("Submit");
         GridPane.setConstraints(submit, 1, 0);
-        
+
         Button clear = new Button("Clear");
         GridPane.setConstraints(clear, 1, 1);
-        
+
         Label label = new Label();
         GridPane.setConstraints(label, 0, 7);
         GridPane.setColumnSpan(label, 2);
 
         //Setting an action for the Submit button
-
         submit.setOnAction((ActionEvent) -> {
             if ((email.getText() != null && !email.getText().isEmpty())) {
                 label.setText(name.getText() + " " + lastName.getText() + ", "
                         + "Welcome to the team, you have been added to the system as a pilot!");
+                gui.getCore().doRegisterPilot(name.getText(), lastName.getText(),
+                        email.getText(), employeeNumber.getText(),
+                        sertificate.getText());
 
             } else {
                 label.setText("You have not registered yet!.");
@@ -254,7 +257,6 @@ public class RegisterNewStage {
         });
 
         //Setting an action for the Clear button
-        
         clear.setOnAction((ActionEvent) -> {
             name.clear();
             lastName.clear();
@@ -263,10 +265,11 @@ public class RegisterNewStage {
             sertificate.clear();
             label.setText(null);
         });
-        grid.getChildren().addAll(name, lastName, email, employeeNumber, sertificate,  submit, clear, label);
+        grid.getChildren().addAll(name, lastName, email, employeeNumber, sertificate, submit, clear, label);
         pilotScene = new Scene(grid, 850, 500);
     }
-          private void createRegisterFlightScene() {
+
+    private void createRegisterFlightScene() {
 
         GridPane grid = new GridPane();
         grid.setPadding(new Insets(20, 20, 20, 20));
@@ -274,50 +277,59 @@ public class RegisterNewStage {
         grid.setVgap(10);
         grid.setHgap(10);
         grid.setStyle("-fx-font-size: 30;");
-        
+
         TextField flightID = new TextField();
         flightID.setPromptText("Enter flight ID.");
         flightID.setPrefColumnCount(10);
         GridPane.setConstraints(flightID, 0, 0);
-        
+
         TextField arrivalHour = new TextField();
         arrivalHour.setPrefColumnCount(5);
         arrivalHour.setPromptText("Enter hour of arrival");
         GridPane.setConstraints(arrivalHour, 0, 1);
-        
+
         TextField arrivalMin = new TextField();
         arrivalMin.setPrefColumnCount(5);
         arrivalMin.setPromptText("Enter minute of arrival");
         GridPane.setConstraints(arrivalMin, 1, 1);
-        
+
         TextField departureHour = new TextField();
         departureHour.setPrefColumnCount(5);
         departureHour.setPromptText("Enter hour of departure");
         GridPane.setConstraints(departureHour, 0, 3);
-        
+
         TextField departureMin = new TextField();
         departureMin.setPrefColumnCount(5);
         departureMin.setPromptText("Enter minute of departure");
         GridPane.setConstraints(departureMin, 1, 3);
+        
+        TextField planeType = new TextField();
+        planeType.setPrefColumnCount(6);
+        departureMin.setPromptText("Enter type of plane");
+        GridPane.setConstraints(departureMin, 0, 5);
 
         Button submit = new Button("Submit");
         GridPane.setConstraints(submit, 3, 0);
-        
+
         Button clear = new Button("Clear");
         GridPane.setConstraints(clear, 3, 1);
-        
+
         Label label = new Label();
         GridPane.setConstraints(label, 0, 6);
         GridPane.setColumnSpan(label, 2);
 
         //Setting an action for the Submit button
-
         submit.setOnAction((ActionEvent) -> {
-           label.setText("Flight has been registered to the system!");
+            label.setText("Flight has been registered to the system!");
+            gui.getCore().doCreateFlight(flightID.getText(), 
+                    Integer.parseInt(departureHour.getText()), 
+                    Integer.parseInt(departureMin.getText()), 
+                    Integer.parseInt(arrivalHour.getText()), 
+                    Integer.parseInt(arrivalMin.getText()),
+                    Integer.parseInt(planeType.getText()));
         });
 
         //Setting an action for the Clear button
-        
         clear.setOnAction((ActionEvent) -> {
             flightID.clear();
             departureHour.clear();
